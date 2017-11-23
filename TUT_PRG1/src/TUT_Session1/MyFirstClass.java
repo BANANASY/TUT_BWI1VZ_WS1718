9de4213af127e550cf6d881c06f6cf2c8e76fbe6
wi15b189 <wi15b189@technikum-wien.at>
2017-11-23 16:01:36 +0100

    initial commit

---
+++ TUT_PRG1/src/TUT_Session1/MyFirstClass.java
@@ -0,0 +1,13 @@
+package TUT_Session1;
+
+import Utils.TextIO;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TextIO.putf("Happy Birthday Chris");
+
+	}
+
+}
